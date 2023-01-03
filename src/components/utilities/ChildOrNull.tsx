04a8cde92237fdf89a28a55ff2020641f6418eb3
Chris <couellette@webstaurantstore.com>
2023-01-02 21:33:15 -0500

    Update <ChildOrNull> utility

--- src/components/utilities/ChildOrNull.tsx
+++ src/components/utilities/ChildOrNull.tsx
@@ -1,13 +1,15 @@
+import { ReactNode } from "react";
+
 export type ChildOrNullProps = {
   condition: boolean;
-  children: JSX.Element;
+  children: ReactNode;
 };
 
 const ChildOrNull = ({
   condition,
   children,
 }: ChildOrNullProps): JSX.Element | null => {
-  return condition ? children : null;
+  return condition ? <>{children}</> : null;
 };
 
 export { ChildOrNull };
