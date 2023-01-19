a3384e9a088e0fc8612f92ac5233eac7ceb000b1
Chris <couellette@webstaurantstore.com>
2023-01-19 18:21:08 -0500

    fix: portal uses global store for mounted dom node

--- src/components/utilities/Portal.tsx
+++ src/components/utilities/Portal.tsx
@@ -1,12 +1,21 @@
 import { ReactNode } from "react";
 import { createPortal } from "react-dom";
+import { createGlobalStore, useStore } from "../..";
+
+export const portalStore = createGlobalStore("#root");
 
 export type PortalProps = {
   elem?: HTMLElement | string;
   children?: ReactNode;
 };
 
-const Portal = ({ children, elem = "#root" }: PortalProps): JSX.Element => {
+const Portal = ({
+  children,
+  elem: controlledElem,
+}: PortalProps): JSX.Element => {
+  const internalElem = useStore(portalStore);
+  const elem = controlledElem ?? internalElem;
+
   const element =
     typeof elem === "string" ? document.querySelector(elem) : elem;
 
