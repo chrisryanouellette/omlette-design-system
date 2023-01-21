f50bfa983a3b1b0e1af464ee448a6b81ab4f4303
Chris <couellette@webstaurantstore.com>
2023-01-21 14:36:57 -0500

    fix: add `onBeforeClose` and `onAfterClose` to drawer trigger

--- src/components/drawer/drawer.trigger.tsx
+++ src/components/drawer/drawer.trigger.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode } from "react";
+import { FC, MouseEvent, ReactNode } from "react";
 import { Button, ButtonProps } from "@Components/button";
 import { isDrawerOpenKey } from "./store";
 
@@ -6,23 +6,31 @@ export type DrawerTriggerProps = ButtonProps & {
   drawer: string;
   element?: FC;
   children?: ReactNode;
+  onBeforeClose?: (e: MouseEvent<HTMLElement>) => void;
+  onAfterClose?: (e: MouseEvent<HTMLElement>) => void;
 };
 
 const DrawerTrigger = ({
   children,
   drawer,
   element,
+  onAfterClose,
+  onBeforeClose,
+  onClick,
   ...rest
 }: DrawerTriggerProps): JSX.Element => {
   const Element = element ?? Button;
 
-  const handleClick = (): void => {
+  const handleClick = (e: MouseEvent<HTMLButtonElement>): void => {
     const openId = isDrawerOpenKey.get();
+    onBeforeClose?.(e);
     if (openId !== drawer) {
       isDrawerOpenKey.set(drawer);
     } else {
       isDrawerOpenKey.set(null);
     }
+    onClick?.(e);
+    onAfterClose?.(e);
   };
 
   return (
