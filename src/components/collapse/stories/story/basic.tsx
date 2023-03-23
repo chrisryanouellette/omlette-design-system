c6ebbcc21f14ee583548f9124e99f3a893397c3a
Chris O <35933106+chrisryanouellette@users.noreply.github.com>
2023-03-23 17:58:33 -0400

    feat: collapse mount children (#32)

--- src/components/collapse/stories/story/basic.tsx
+++ src/components/collapse/stories/story/basic.tsx
@@ -1,18 +1,22 @@
+import { FC } from "react";
 import { Collapse } from "@Components/collapse/collapse";
 import { bindTemplate } from "@Storybook/types";
+import { CollapseControls } from "../collapse.stories";
 
-const BasicCollapseStory = bindTemplate(() => {
-  return (
-    <Collapse>
-      <Collapse.Trigger>
-        Expand Panel
-        <Collapse.Carrot />
-      </Collapse.Trigger>
-      <Collapse.Panel>
-        <h2>Panel Content</h2>
-      </Collapse.Panel>
-    </Collapse>
-  );
-});
+const BasicCollapseStory = bindTemplate<FC<CollapseControls>>(
+  ({ "Unmount Children": unmountChildren }) => {
+    return (
+      <Collapse>
+        <Collapse.Trigger>
+          Expand Panel
+          <Collapse.Carrot />
+        </Collapse.Trigger>
+        <Collapse.Panel unmountChildren={unmountChildren}>
+          <h2>Panel Content</h2>
+        </Collapse.Panel>
+      </Collapse>
+    );
+  }
+);
 
 export { BasicCollapseStory };
