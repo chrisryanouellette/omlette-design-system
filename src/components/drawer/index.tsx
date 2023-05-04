a1bf06dde9c804fc24fe4d557e4c6bd30457c94f
Chris <couellette@webstaurantstore.com>
2023-05-04 17:13:34 -0400

    fix: drawer-store-export

--- src/components/drawer/index.tsx
+++ src/components/drawer/index.tsx
@@ -1,3 +1,4 @@
 export { Drawer } from "./drawer";
+export { drawerStore } from "./store";
 export type { DrawerPanelProps } from "./drawer.panel";
 export type { DrawerTriggerProps } from "./drawer.trigger";
