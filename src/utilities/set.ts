b9da3686769de5ea502593885f0ff829fc00a70d
Chris <couellette@webstaurantstore.com>
2023-05-07 16:05:29 -0400

    fix: is unique set

--- src/utilities/set.ts
+++ src/utilities/set.ts
@@ -1,6 +1,6 @@
 export const isUniqueSet = <T>(a: Set<T>, b: Set<T>): boolean => {
   if (a.size !== b.size) {
-    return false;
+    return true;
   }
   return Array.from(b).some((element) => {
     return !a.has(element);
