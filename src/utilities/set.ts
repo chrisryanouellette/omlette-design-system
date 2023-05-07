460d46a265f9f4445435de7ec3993dd5dab53b2b
Chris <couellette@webstaurantstore.com>
2023-05-07 15:58:55 -0400

    fix: form group

--- src/utilities/set.ts
+++ src/utilities/set.ts
@@ -1,8 +1,8 @@
 export const isUniqueSet = <T>(a: Set<T>, b: Set<T>): boolean => {
   if (a.size !== b.size) {
-    return Array.from(b).every((element) => {
-      return !a.has(element);
-    });
+    return false;
   }
-  return false;
+  return Array.from(b).some((element) => {
+    return !a.has(element);
+  });
 };
