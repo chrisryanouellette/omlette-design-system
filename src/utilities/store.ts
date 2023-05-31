b8a93c553dd81b8daf602b92d011e0f4b7b464db
Chris O <35933106+chrisryanouellette@users.noreply.github.com>
2023-05-31 14:59:47 -0400

    feat: store reset (#110)

--- src/utilities/store.ts
+++ src/utilities/store.ts
@@ -5,6 +5,7 @@ export type GlobalStoreKey<Store> = {
 };
 export type GetStore<Store> = () => Store;
 export type SetStore<Action> = (update: Action) => void;
+export type ResetActon = () => void;
 export type SubscribeToStore<Store> = (
   cb: (store: Store) => void
 ) => () => void;
@@ -12,6 +13,7 @@ export type SubscribeToStore<Store> = (
 export type UseCreateStore<Store, Action = Partial<Store>> = {
   get: GetStore<Store>;
   set: SetStore<Action>;
+  reset: ResetActon;
   subscribe: SubscribeToStore<Store>;
 };
 
@@ -41,6 +43,10 @@ export const createGlobalStore = <Store, Action = Partial<Store>>(
     globalStore.set(key, update);
     key.subscriptions.forEach((sub) => sub(get()));
   };
+  const reset: UseCreateStore<Store>["reset"] = () => {
+    globalStore.set(key, value);
+    key.subscriptions.forEach((sub) => sub(get()));
+  };
   const subscribe: UseCreateStore<Store>["subscribe"] = (cb) => {
     key.subscriptions.add(cb);
     /*
@@ -52,7 +58,7 @@ export const createGlobalStore = <Store, Action = Partial<Store>>(
     cb(get());
     return () => key.subscriptions.delete(cb);
   };
-  return { get, set, subscribe };
+  return { get, set, reset, subscribe };
 };
 
 /*
@@ -80,12 +86,21 @@ export const useCreateStore = <Store, Action = Partial<Store>>(
     [reducer]
   );
 
+  const reset = useCallback<ResetActon>(() => {
+    store.current = initial;
+    /** Notify all subscription when store is set */
+    subscriptions.current.forEach((sub) => sub(store.current));
+  }, [initial]);
+
   const subscribe = useCallback<SubscribeToStore<Store>>((cb) => {
     subscriptions.current.add(cb);
     return () => subscriptions.current.delete(cb);
   }, []);
 
-  return useMemo(() => ({ get, set, subscribe }), [get, set, subscribe]);
+  return useMemo(
+    () => ({ get, set, reset, subscribe }),
+    [get, reset, set, subscribe]
+  );
 };
 
 export type SelectorFn<Store, Selected = Store> = (
