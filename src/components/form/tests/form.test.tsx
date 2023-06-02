5ea7b9f4a76a096e0805cdebb3ddd50c1dd43adb
Chris <couellette@webstaurantstore.com>
2023-06-02 16:56:56 -0400

    feat: form test

---
+++ src/components/form/tests/form.test.tsx
@@ -0,0 +1,113 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import {
+  Button,
+  Form,
+  FormField,
+  TextInput,
+  ValidationAddError,
+} from "@Components/index";
+
+const finishMock = jest.fn();
+const finishFailedMock = jest.fn();
+const validationMock = jest.fn(
+  (field: FormField<string>, addError: ValidationAddError) => {
+    if (field.value === "ERROR") {
+      addError("Error Message");
+    }
+  }
+);
+
+beforeEach(() => {
+  finishMock.mockClear();
+  finishFailedMock.mockClear();
+  validationMock.mockClear();
+});
+
+describe("Form component", () => {
+  test("can have a defaultValue", async () => {
+    render(
+      <Form onFinish={finishMock}>
+        <Form.Item name="test" defaultValue="Name">
+          <TextInput label="Test" />
+        </Form.Item>
+        <Button>Submit</Button>
+      </Form>
+    );
+
+    await userEvent.click(screen.getByRole("button"));
+    expect(finishMock.mock.calls.length).toBe(1);
+    expect(finishMock.mock.calls[0][0]).toMatchInlineSnapshot(`
+      {
+        "test": {
+          "defaultValue": "Name",
+          "errors": Set {},
+          "touched": false,
+          "value": "Name",
+        },
+      }
+    `);
+  });
+
+  test("can be submitted", async () => {
+    render(
+      <Form onFinish={finishMock}>
+        <Form.Item name="test">
+          <TextInput label="Test" />
+        </Form.Item>
+        <Button>Submit</Button>
+      </Form>
+    );
+
+    await userEvent.type(screen.getByLabelText(/test/i), "Name");
+    await userEvent.click(screen.getByRole("button"));
+    expect(finishMock.mock.calls.length).toBe(1);
+    expect(finishMock.mock.calls[0][0]).toMatchInlineSnapshot(`
+      {
+        "test": {
+          "defaultValue": null,
+          "errors": Set {},
+          "touched": true,
+          "value": "Name",
+        },
+      }
+    `);
+  });
+
+  test("can fail to submit", async () => {
+    render(
+      <Form onFinish={finishMock} onFinishFailed={finishFailedMock}>
+        <Form.Item name="test" validation={validationMock}>
+          <TextInput label="Test" />
+        </Form.Item>
+        <Button>Submit</Button>
+      </Form>
+    );
+
+    await userEvent.type(screen.getByLabelText(/test/i), "ERROR");
+    await userEvent.click(screen.getByRole("button"));
+    await screen.findByText("Error Message");
+    expect(screen.getByText("Error Message")).toBeInTheDocument();
+  });
+
+  test("can fail to submit then succeed to submit", async () => {
+    render(
+      <Form onFinish={finishMock} onFinishFailed={finishFailedMock}>
+        <Form.Item name="test" validation={validationMock}>
+          <TextInput label="Test" />
+        </Form.Item>
+        <Button>Submit</Button>
+      </Form>
+    );
+
+    await userEvent.type(screen.getByLabelText(/test/i), "ERROR");
+    await userEvent.click(screen.getByRole("button"));
+    await screen.findByText("Error Message");
+    expect(screen.getByText("Error Message")).toBeInTheDocument();
+
+    await userEvent.type(screen.getByLabelText(/test/i), "Name");
+    await userEvent.click(screen.getByRole("button"));
+
+    expect(screen.queryByText("Error Message")).not.toBeInTheDocument();
+  });
+});
